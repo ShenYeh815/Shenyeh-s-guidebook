6a3cf8b9a816c3eb85b7d450e1207d770cc7662b
Wes Yeh <ywc0919@WesdeMacBook-Pro.local>
2021-12-22 15:05:24 +0800

    update_211222

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Shenyeh's guidebook",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
